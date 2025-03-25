a490aa423be99066372691b3a38ab3d1eb796914
Danirug <daniru259@gmail.com>
2025-03-26 01:06:35 +0530

    deleted code

--- lib/Dashboard_Screens/MainDashboard_Screen.dart
+++ lib/Dashboard_Screens/MainDashboard_Screen.dart
@@ -7,21 +7,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Dashboard_Screen(),
-    );
-  }
-}
-
 class Dashboard_Screen extends StatefulWidget {
   const Dashboard_Screen({super.key});
 
